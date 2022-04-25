9658c3a8865a0986e5e68606a6f99989b908f0ce
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-25 11:29:00 +0530

    Added: Player To Choose letter X or O

--- src/com/bridglabz/TicTac.java
+++ src/com/bridglabz/TicTac.java
@@ -10,13 +10,28 @@ public class TicTac {
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game....!!!");
         board();
+        userinput();
     }
-        private static void board() {
+        public static void board() {
             element = new char[10];
             for(int i=1; i<10; i++) {
-                element[i]='.';
+                element[i]=' ';
             }
         }
+    public static void userinput() {
+        Scanner in=new Scanner(System.in);
+        System.out.println(" Select Your Marker (O or X): ");
+        char player=in.next().charAt(0);
+        char computer;
+        if (player=='X') {
+            computer='O';
+            System.out.println("User mark is :" +player);}
+
+        else
+            computer='X';
+        System.out.println("User mark is :" +player);
     }
+}
+
 
 
