13f185c179be4ca17b8b17eeba270f3294a7586e
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-26 23:09:17 +0530

    Added: Computer to play like player.

--- src/com/bridglabz/TicTac.java
+++ src/com/bridglabz/TicTac.java
@@ -10,7 +10,7 @@ public class TicTac {
 
     private  static  char computer;
     private static int moves=0;
-
+    private static int flag=0;
     private static boolean winner=false;
 
     public static void main(String[] args) {
@@ -26,7 +26,7 @@ public class TicTac {
     public static void board() {
         element = new char[10];
         for (int i = 0; i < 9; i++) {
-            element[i] = '+';
+            element[i] = ' ';
         }
     }
 
@@ -34,20 +34,24 @@ public class TicTac {
         Scanner in = new Scanner(System.in);
         System.out.println(" Select Your Marker (O or X): ");
         player = in.next().charAt(0);
-        if (player == 'X') {
-            computer = 'O';
+        if (player == 'x') {
+            computer = 'o';
             System.out.println("User mark is :" + player);
-        } else
-            computer = 'X';
-        System.out.println("User mark is :" + player);
+        } else{
+            computer='x';
+            player='o';
+            System.out.println("Computer mark is:"+computer);
+        }
     }
 
     public  static void toss(){
         int result=(int)Math.floor(Math.random() * 10) % 2;
         if (result==1){
+            flag=0;
             System.out.println("Player has won and he will play first");
         }
         else{
+            flag=1;
             System.out.println("Computer will play first");
         }
     }
@@ -70,44 +74,65 @@ public class TicTac {
         }
     }
 
-    public static void winningConditions(){
-            if (element[0]==element[1] && element[1]==element[2] && element[2]==player){
+    public static void winningConditions(char playerletter){
+            if (element[0]==element[1] && element[1]==element[2] && element[2]==playerletter){
                 winner=true;
-            } else if(element[3]==element[4] && element[4]==element[5] && element[5]==player){
+            } else if(element[3]==element[4] && element[4]==element[5] && element[5]==playerletter){
                 winner=true;
-            } else if(element[6]==element[7] && element[7]==element[8] && element[8]==player){
+            } else if(element[6]==element[7] && element[7]==element[8] && element[8]==playerletter){
                 winner=true;
-            } else if (element[0]==element[3] && element[3]==element[6] && element[6]==player) {
+            } else if (element[0]==element[3] && element[3]==element[6] && element[6]==playerletter) {
                 winner=true;
-            } else if (element[1]==element[4] && element[4]==element[7] && element[7]==player) {
+            } else if (element[1]==element[4] && element[4]==element[7] && element[7]==playerletter) {
                 winner=true;
-            } else if (element[2]==element[5] && element[5]==element[8] && element[8]==player) {
+            } else if (element[2]==element[5] && element[5]==element[8] && element[8]==playerletter) {
                 winner=true;
-            } else if (element[0]==element[4] && element[4]==element[8] && element[8]==player) {
+            } else if (element[0]==element[4] && element[4]==element[8] && element[8]==playerletter) {
                 winner=true;
-            } else if (element[2]==element[4] && element[4]==element[6] && element[6]==player) {
+            } else if (element[2]==element[4] && element[4]==element[6] && element[6]==playerletter) {
                 winner=true;
             }
     }
 
     public static void gamePlay(){
         while(moves < 9){
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Please enter the position between 0-8");
-            int position=sc.nextInt();
-            if(element[position]=='+'){
-                element[position]=player;
-                printBoard();
-                moves++;
-                winningConditions();
-                if(winner==true){
-                    System.out.println("Player has won");
-                    System.exit(0);
+            if(flag==0){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Please enter the position between 0-8");
+                int position=sc.nextInt();
+                if(element[position]==' '){
+                    element[position]=player;
+                    printBoard();
+                    moves++;
+                    winningConditions(player);
+                    if(winner==true){
+                        System.out.println("Player has won");
+                        System.exit(0);
+                    }
+                    flag=1;
+                    gamePlay();
                 }
-                gamePlay();
-            }
-            else {
-                System.out.println("Invalid move");
+                else {
+                    System.out.println("Invalid move");
+                }
+            } else if (flag==1) {
+                System.out.println("Computer turn");
+                int position=(int)Math.floor(Math.random() * 10) % 9;
+                if(element[position]==' '){
+                    element[position]=computer;
+                    printBoard();
+                    moves++;
+                    winningConditions(computer);
+                    if(winner==true){
+                        System.out.println("Computer has won");
+                        System.exit(0);
+                    }
+                    flag=0;
+                    gamePlay();
+                }
+                else {
+                    System.out.println("Invalid move");
+                }
             }
         }
     }
